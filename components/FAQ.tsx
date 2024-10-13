605a6248b31e13490d4e2faf289f8223efc48b33
Natnael Mulugeta <nati2mulugeta@gmail.com>
2024-10-13 16:57:19 +0300

    Fix: FAQ section

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -13,7 +13,7 @@ const FAQ = () => {
   const handleOpen = (value: number) => setOpen(open === value ? 0 : value);
 
   return (
-    <section className="w-full py-20 max-w-inner flex flex-col justify-center items-start mx-auto">
+    <section className="w-4/5 md:w-full py-20 max-w-inner flex flex-col justify-center items-start mx-auto">
       <Typography
         variant="small"
         color="blue"
@@ -27,20 +27,26 @@ const FAQ = () => {
       >
         Frequently <span className="text-mediumGray">asked</span> question
       </Typography>
-      <div className="relative w-full flex flex-col gap-9 justify-center items-center mt-14">
+      <div className="relative w-full flex flex-col gap-2 md:gap-9 justify-center items-center mt-14">
         {[1, 2, 3, 4].map((i) => (
           <Accordion
             key={i}
             open={open === i}
-            icon={open ? <ChevronUp /> : <ChevronDown />}
+            icon={
+              open ? (
+                <ChevronUp className="w-4 md:w-16" />
+              ) : (
+                <ChevronDown className="w-4 md:w-16" />
+              )
+            }
           >
             <AccordionHeader
-              className="font-gilroy-medium font-normal text-xl"
+              className="font-gilroy-medium font-normal text-sm md:text-xl"
               onClick={() => handleOpen(i)}
             >
               How to reset my password ?{" "}
             </AccordionHeader>
-            <AccordionBody>
+            <AccordionBody className="text-xs md:text-sm">
               We&apos;re not always in the position that we want to be at.
               We&apos;re constantly growing. We&apos;re constantly making
               mistakes. We&apos;re constantly trying to express ourselves and
@@ -50,30 +56,6 @@ const FAQ = () => {
         ))}
       </div>
     </section>
-
-    // <section className="relative w-full bg-red-50 px-8 mb-4  mt-72">
-    //   <div className="bg-yellow-100  mx-auto py-44 w-full max-w-7xl text-center">
-    //     <svg
-    //       className="overflow-hidden w-[338px ] h-[146px] md:w-[1109px] md:h-[485px] mx-auto"
-
-    //       viewBox="0 0 1109 485"
-    //       xmlns="http://www.w3.org/2000/svg"
-    //     >
-    //       <defs>
-    //         <clipPath id="clip-path">
-    //           <path d="M45 484.209C20.1472 484.209 0 464.062 0 439.209V179.119C0 154.266 20.1471 134.119 45 134.119H418.158C443.011 134.119 463.158 113.972 463.158 89.1189V45C463.158 20.1472 483.305 0 508.158 0H1063.25C1088.11 0 1108.25 20.1472 1108.25 45V312.883C1108.25 337.736 1088.11 357.883 1063.25 357.883H691.369C666.516 357.883 646.369 378.03 646.369 402.883V439.209C646.369 464.062 626.222 484.209 601.369 484.209H45Z" />
-    //         </clipPath>
-    //       </defs>
-    //       <image
-    //         href="/bcd2.png"
-    //         width="1109"
-    //         height="485"
-    //         clipPath="url(#clip-path)"
-    //         preserveAspectRatio="xMidYMid slice"
-    //       />
-    //     </svg>
-    //   </div>
-    // </section>
   );
 };
 
